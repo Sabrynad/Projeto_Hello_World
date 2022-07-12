2da06dd82123e2292ff2df17c4e74453330a7a74
Sabryna <sabrynadeangelo@gmail.com>
2022-07-12 17:39:23 -0300

    Lista 1 C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projeto_Hello_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
